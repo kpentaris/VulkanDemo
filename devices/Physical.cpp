e0c61963147d2fd412d66ce80b97e1bc932131fc
kpentaris <kpentaris@gmail.com>
2020-10-31 01:23:41 +0200

    small update on device detection

--- devices/Physical.cpp
+++ devices/Physical.cpp
@@ -9,6 +9,25 @@ bool pickPhysicalDevice(VkInstance instance) {
   }
   std::vector<VkPhysicalDevice> devices(deviceCount); 
   vkEnumeratePhysicalDevices(instance, $deviceCount, devices.data());
+
+  for (const auto& device : devices) {
+    if (isDeviceSuitable(device)) {
+      physicalDevice = device;
+      break;
+    }
+  }
+
+  if (physicalDevice == VK_NULL_HANDLE) {
+    std::cout << "Failed to find a suitable GPU!" << std::endl;
+    return VK_ERROR_INITIALIZATION_FAILED;
+  }
 }
 
-bool isDeviceSuitable(VkPhysicalDevice
+bool isDeviceSuitable(VkPhysicalDevice device) {
+  VkPhysicalDeviceProperties deviceProperties;
+  vkGetPhysicalDeviceProperties(device, &deviceProperties);
+
+  VkPhysicalDeviceFeatures deviceFeatures;
+  vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+  return true;
+}
